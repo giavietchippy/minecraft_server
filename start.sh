668b5673522b8e3293b8e989e9a1fb59805d56c6
giavietchippy <giavietchippy@gmail.com>
2025-10-19 14:47:32 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-echo "Starting Minecraft server..."
+if [ ! -f server.jar ]; then
+  echo "Downloading Minecraft server..."
+  curl -L https://piston-data.mojang.com/v1/objects/95495a7f485eedd84ce928cef5e223b757d2f764/server.jar -o server.jar
+fi
 java -Xmx1024M -Xms1024M -jar server.jar nogui
